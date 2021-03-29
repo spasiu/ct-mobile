16ef0d17dd3fcfe62833f15c4218558d85cd3e0f
Cecilia Hunka <80286563+l-hnka@users.noreply.github.com>
2021-03-29 16:31:38 -0400

    i18n (#38)
    * installing i18n libraries
    
    * adding i18n config

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { enableScreens } from 'react-native-screens';
+import * as RNLocalize from 'react-native-localize';
 
 import './styles/tachyons';
 import { RootNavigator } from './navigators/root-navigator';
+import { setI18nConfig } from './i18n/i18n';
 
 // for performance optimizations and native feel
 // https://reactnavigation.org/docs/react-native-screens
 enableScreens();
 
-const App = () => <RootNavigator />;
+const App = () => {
+  useEffect(() => {
+    setI18nConfig();
+    RNLocalize.addEventListener('change', setI18nConfig);
+    return () => {
+      RNLocalize.removeEventListener('change', setI18nConfig);
+    };
+  }, []);
+
+  return <RootNavigator />;
+};
 
 export default App;
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18n-js';
+import * as RNLocalize from 'react-native-localize';
+import { memoizeWith } from 'ramda';
+
+export const translations: { [key: string]: () => object } = {
+  en: () => require('./locales/en.json'),
+};
+
+export const DEFAULT_LANGUAGE = 'en';
+export const SUPPORTED_LANGUAGES = Object.keys(translations);
+
+export const t = memoizeWith(
+  (key, config) =>
+    config
+      ? i18n.currentLocale() + key + JSON.stringify(config)
+      : i18n.currentLocale() + key,
+  (key, config) => i18n.t(key, config),
+);
+
+export const setI18nConfig = () => {
+  const { languageTag } = RNLocalize.findBestAvailableLanguage(
+    SUPPORTED_LANGUAGES,
+  ) || { languageTag: DEFAULT_LANGUAGE };
+
+  i18n.translations = { [languageTag]: translations[languageTag]() };
+  i18n.locale = languageTag;
+};
