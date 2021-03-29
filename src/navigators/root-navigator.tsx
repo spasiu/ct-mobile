d8059b57e66efd9bb2fc28f11cf21eaec644757d
Cecilia Hunka <80286563+l-hnka@users.noreply.github.com>
2021-03-28 20:00:30 -0400

    Splash and icons (#25)
    * adding splash to ios
    
    * adding android splash
    
    * updating yark lock and clean scripts
    
    * adding icons to ios
    
    * adding android icons

--- src/navigators/root-navigator.tsx
+++ src/navigators/root-navigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 import { ProtectedStack } from './stacks/protected-stack';
 import { PublicStack } from './stacks/public-stack';
@@ -14,28 +15,31 @@ type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const isLoggedIn = true;
 
-export const RootNavigator = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      {isLoggedIn ? (
-        <Stack.Screen
-          name={ROUTES_IDS.PROTECTED_STACK}
-          component={ProtectedStack}
-          options={{
-            headerShown: false,
-          }}
-        />
-      ) : (
-        <Stack.Screen
-          name={ROUTES_IDS.PUBLIC_STACK}
-          component={PublicStack}
-          options={{
-            headerShown: false,
-          }}
-        />
-      )}
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+export const RootNavigator = () => {
+  SplashScreen.hide();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isLoggedIn ? (
+          <Stack.Screen
+            name={ROUTES_IDS.PROTECTED_STACK}
+            component={ProtectedStack}
+            options={{
+              headerShown: false,
+            }}
+          />
+        ) : (
+          <Stack.Screen
+            name={ROUTES_IDS.PUBLIC_STACK}
+            component={PublicStack}
+            options={{
+              headerShown: false,
+            }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 RootNavigator.displayName = 'RootNavigator';
