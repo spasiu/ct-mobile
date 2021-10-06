f1b67ea2f810cedf3f16886667226243d194e413
Scott A Morin <scottanthonymorin@gmail.com>
2021-10-06 09:42:09 -0400

    minor tweaks to code

--- src/components/address-prediction-list/address-prediction-list.tsx
+++ src/components/address-prediction-list/address-prediction-list.tsx
@@ -3,7 +3,7 @@ import { View, TouchableOpacity, Text } from 'react-native';
 import { ADDRESS_FORM_FIELDS } from '../../common/address/address-form';
 import { getPostalCode, PredictionType } from '../../services/places-api';
 import { indexedMap } from '../../utils/ramda';
-import { split, trim } from 'ramda';
+import { split } from 'ramda';
 
 import {
   containerStyle,
@@ -17,43 +17,36 @@ export const AddressPredictionList = ({
   addressPredictions = [],
   onItemPressed = () => undefined,
 }: AddressPredictionListProps): JSX.Element => {
+  const handleOnPress = async (prediction: PredictionType) => {
+    const address = prediction.description;
+    const addressDivided = split(',', address);
+    const addressFirstLine = addressDivided[0]?.trim() || '';
+    const addressCity = addressDivided[1]?.trim() || '';
+    const addressState = addressDivided[2]?.trim() || '';
+    const postalCode = await getPostalCode(prediction.place_id);
+
+    const addressSuggestion = {
+      [ADDRESS_FORM_FIELDS.FIRST_LINE]: addressFirstLine,
+      [ADDRESS_FORM_FIELDS.CITY]: addressCity,
+      [ADDRESS_FORM_FIELDS.STATE_PROVINCE_REGION]: addressState,
+      [ADDRESS_FORM_FIELDS.POSTAL_CODE]: postalCode,
+    };
+
+    onItemPressed(addressSuggestion);
+  };
+
   return (
     <View style={containerStyle}>
       {indexedMap((addressPrediction, index) => {
         const isFirstItem = index === 0;
         const isLastItem = index === addressPredictions.length - 1;
         const prediction = addressPrediction as PredictionType;
+
         return (
           <View key={prediction.place_id}>
             <TouchableOpacity
               style={isFirstItem ? firstRowSpacing : defaultRowSpacing}
-              onPress={async () => {
-                const address = prediction.description;
-
-                const addressDivided = split(',', address);
-
-                const addressFirstLine =
-                  (addressDivided[0] && trim(addressDivided[0])) || '';
-
-                const addressCity =
-                  (addressDivided[1] && trim(addressDivided[1])) || '';
-
-                const addressState =
-                  (addressDivided[2] && trim(addressDivided[2])) || '';
-
-                const postalCode = await getPostalCode(
-                  prediction.place_id,
-                  addressState,
-                );
-
-                const addressSuggestion = {
-                  [ADDRESS_FORM_FIELDS.FIRST_LINE]: addressFirstLine,
-                  [ADDRESS_FORM_FIELDS.CITY]: addressCity,
-                  [ADDRESS_FORM_FIELDS.STATE_PROVINCE_REGION]: addressState,
-                  [ADDRESS_FORM_FIELDS.POSTAL_CODE]: postalCode,
-                };
-                onItemPressed(addressSuggestion);
-              }}>
+              onPress={() => handleOnPress(prediction)}>
               <Text>{prediction.description}</Text>
             </TouchableOpacity>
             {isLastItem ? null : <View style={dividerStyle} />}
--- src/services/places-api/api.ts
+++ src/services/places-api/api.ts
@@ -31,14 +31,17 @@ export const getPredictions = async (
   }
 };
 
-export const getPostalCode = async (place_id: string, state: string) => {
+export const getPostalCode = async (place_id: string) => {
   try {
     const result = await axios.get(
-      `${GOOGLE_PLACES_API_BASE_URL}/details/json?key=${Config.GOOGLE_PLACES_API_KEY}&place_id=${place_id}&fields=formatted_address`,
+      `${GOOGLE_PLACES_API_BASE_URL}/details/json?key=${Config.GOOGLE_PLACES_API_KEY}&place_id=${place_id}`,
     );
-    return result.data.result.formatted_address
-      .split(', ')[2]
-      .replace(`${state} `, '');
+    const postalCode = result.data.result.address_components.filter(
+      (item: { types: string | string[] }) =>
+        item.types.includes('postal_code'),
+    )[0].long_name;
+
+    return postalCode;
   } catch (e) {
     console.log(e);
     return '';
