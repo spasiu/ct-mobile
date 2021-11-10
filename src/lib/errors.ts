dacc6396f755cb7f0dbc8f9204314a757ec27646
Anastasi Bakolias <2235885+spasiu@users.noreply.github.com>
2021-11-10 09:27:03 -0500

    Write a central error handler that logs and displays the error in one place. Also make the error banner stay for 10 seconds instead of 2, and be dismissed on press.

---
+++ src/lib/errors.ts
@@ -0,0 +1,39 @@
+import { MessageType, showMessage } from 'react-native-flash-message';
+import { MessageTypes } from 'subscriptions-transport-ws';
+import { string } from 'yup/lib/locale';
+import { t } from '../i18n/i18n';
+
+const classifyError = (
+  error: Error,
+): { message: string; type: MessageType } => {
+  switch (error) {
+    // add more cases
+    default:
+      return {
+        message: t('errors.generic') || 'An error occured.',
+        type: 'danger' as MessageType,
+      };
+  }
+};
+
+const displayError = (message: string, type: MessageType) => {
+  showMessage({
+    message,
+    type,
+    hideOnPress: true,
+    autoHide: true, // automatically dismiss message after 10 secs
+    duration: 10000, // ms
+  });
+};
+
+export const handleError = (
+  error: Error,
+  message: string | undefined = undefined,
+  type: MessageType | undefined = undefined,
+) => {
+  const details = classifyError(error);
+  message = message || details?.message;
+  type = type || details?.type;
+  displayError(message, type);
+  console.error(error);
+};
