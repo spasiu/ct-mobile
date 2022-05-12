f6eadc3acf394f734df7255871bf6ef8cca91196
Jonathan Weiss <gneeri@gmail.com>
2022-05-12 11:51:05 -0400

    updated memorabilia spelling

--- src/common/hit/hit-selectors.ts
+++ src/common/hit/hit-selectors.ts
@@ -44,7 +44,7 @@ export const hitDescription = (hit: Partial<Hits>): string => {
     hit.parallel,
     hit.insert,
     hit.autograph ? 'Autograph' : '',
-    hit.memoribilia ? hit.memoribilia : '',
+    hit.memorabilia ? hit.memorabilia : '',
     hit.rookie_card ? 'Rookie' : '',
     hit.numbered ? '/' + hit.numbered : '',
   ].filter(h => h).join(' ');
